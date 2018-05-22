24df3aaa5eccb378514096bf40eb52374cc8cdee
Marko <hermes.technic@gmail.com>
2018-05-22 06:44:50 +0200

    commiting 22.05.2018.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-//use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 class AuthController extends Controller
@@ -80,4 +80,6 @@ class AuthController extends Controller
             'expires_in' => auth()->factory()->getTTL() * 60
         ]);
     }
+
+ 
 }
--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::group([
 
     'middleware' => 'api',
+    'namespace' => 'App\Http\Controllers',
     'prefix' => 'auth'
-
 ], function ($router) {
 
     Route::post('login', 'AuthController@login');
